767e42a80036527588ac688e3aabbeae00d91cda
chengwen.zheng <chengwen.zheng@weimob.com>
2024-02-27 17:36:55 +0800

    feat: DP

--- crates/demo_algorithms/src/main.rs
+++ crates/demo_algorithms/src/main.rs
@@ -1,38 +1,44 @@
 use std::cmp::{max, min};
 use std::sync::{Arc, Mutex};
-use std::thread;
+use std::{char, thread};
 
 fn main() {
     let s1 = "abcde";
     let s2 = "ace";
-    let chars1: Vec<char> = s1.chars().collect();
-    let chars2: Vec<char> = s2.chars().collect();
+
+    let chars1: Arc<Vec<char>> = Arc::new(s1.chars().collect());
+    let chars2: Arc<Vec<char>> = Arc::new(s2.chars().collect());
 
     let m = chars1.len();
     let n = chars2.len();
 
-    let round = 2 * max(m, n) - 1;
+    let round = m + n;
     let dp = Arc::new(Mutex::new(vec![vec![0; m + 1]; n + 1]));
-    let mut handles: Vec<thread::JoinHandle<()>> = vec![];
 
     for i in 0..round {
-        let dp_clone = Arc::clone(&dp);
-
+        let dp_clone: Arc<Mutex<Vec<Vec<i32>>>> = Arc::clone(&dp);
+        let mut handles: Vec<thread::JoinHandle<()>> = vec![];
+        print!("==============={}: ", i);
+        let chars1_clone: Arc<Vec<char>> = chars1.clone();
+        let chars2_clone = chars2.clone();
         let handle = thread::spawn(move || {
-            // todo: 将longest_common_subsequence分配给多个线程执行
             // 1. 计算出本轮能够计算的单元格
             let mut dp = dp_clone.lock().unwrap();
+            for j in (0..min(i + 1, m)).rev() {
+                let index: usize = i - j;
 
-            for j in (0..min(i, m)) {
-                let index = i - j;
-                println!("j:i({}{})", j, index);
+                println!("j:index({}{})", j, index);
             }
         });
         // 2. 将任务分配给线程执行
         handles.push(handle);
         // 3. 等待线程执行完毕
+        for handle in handles {
+            handle.join().unwrap();
+        }
     }
-    for handle in handles {
-        handle.join().unwrap();
-    }
+
+    let dp = dp.lock().unwrap();
+    println!("{:?}", dp[n][m]);
+    println!("{:?}", dp);
 }
