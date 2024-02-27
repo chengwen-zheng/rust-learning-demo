9b4594482d9c6849b09bc09e9485dda59fe2e5d8
chengwen-zheng <chengwenzheng1222@gmail.com>
2024-02-27 11:27:17 +0800

    feat: handle

--- crates/demo_algorithms/src/main.rs
+++ crates/demo_algorithms/src/main.rs
@@ -1,27 +1,38 @@
-fn longest_common_subsequence(s1: &str, s2: &str) -> i32 {
+use std::cmp::max;
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+fn main() {
+    let s1 = "abcde";
+    let s2 = "ace";
     let chars1: Vec<char> = s1.chars().collect();
     let chars2: Vec<char> = s2.chars().collect();
 
     let m = chars1.len();
     let n = chars2.len();
-    let mut dp: Vec<Vec<i32>> = vec![vec![0; m]; n];
 
-    for i in 0..(n - 1) {
-        for j in 0..(m - 1) {
-            if chars1[j] == chars2[i] {
-                dp[i + 1][j + 1] = dp[i][j] + 1;
-            } else {
-                dp[i + 1][j + 1] = dp[i][j + 1].max(dp[i + 1][j]);
+    let round = 2 * max(m, n) - 1;
+    let dp = Arc::new(Mutex::new(vec![vec![0; m + 1]; n + 1]));
+    let mut handles: Vec<thread::JoinHandle<()>> = vec![];
+
+    for i in 0..round {
+        let dp_clone = Arc::clone(&dp);
+
+        let handle = thread::spawn(move || {
+            // todo: 将longest_common_subsequence分配给多个线程执行
+            // 1. 计算出本轮能够计算的单元格
+            let mut dp = dp_clone.lock().unwrap();
+
+            for j in (0..m) {
+                let index = i.checked_sub(j).unwrap_or(usize::MAX);
+                println!("j:i({}{})", j, index);
             }
-        }
+        });
+        // 2. 将任务分配给线程执行
+        handles.push(handle);
+        // 3. 等待线程执行完毕
     }
-
-    dp[n - 1][m - 1]
-}
-
-fn main() {
-    let s1 = "abcde";
-    let s2 = "ace";
-
-    println!("lcs:{}", longest_common_subsequence(s1, s2));
+    for handle in handles {
+        handle.join().unwrap();
+    }
 }
